ad799565fb44ec3f36a6cfafa327285c0042e897
piyumalBhathiya <139671996+piyumalBhathiya@users.noreply.github.com>
2025-03-15 17:51:46 +0000

    Question four done

--- One.java
+++ One.java
@@ -16,5 +16,20 @@ public class One{
         for (int i=10; i>=1; i--){
             System.out.println(i);
         }
+
+        System.out.println("----------------------------------------");
+
+        //Q3
+        for (int i=10; i>=1; i--){
+            System.out.println(i);
+        }
+
+        System.out.println("----------------------------------------");
+        //Q4
+        for (int i=1; i<=20; i++){
+            if (i%2==1){
+                System.out.println(i);
+            }            
+        }
     }
 }

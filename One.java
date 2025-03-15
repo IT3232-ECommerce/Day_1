c9824b4b2677bc263668ba544372c160ca8efb07
piyumalBhathiya <139671996+piyumalBhathiya@users.noreply.github.com>
2025-03-15 17:19:35 +0000

    Question three done

--- One.java
+++ One.java
@@ -4,9 +4,16 @@ public class One{
         System.out.println("Hello World...");
 
         System.out.println("----------------------------------------");
-        
+
         //Q2
         for (int i=1; i<=10; i++){
+            System.out.println(i);
+        }
+
+        System.out.println("----------------------------------------");
+
+        //Q3
+        for (int i=10; i>=1; i--){
             System.out.println(i);
         }
     }

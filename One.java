42e00bfcc505f047401b36e4853196d48aa82dd7
piyumalBhathiya <139671996+piyumalBhathiya@users.noreply.github.com>
2025-03-15 17:17:39 +0000

    Question two done

--- One.java
+++ One.java
@@ -2,5 +2,12 @@ public class One{
     public static void main(String [] args){
         //Q1
         System.out.println("Hello World...");
+
+        System.out.println("----------------------------------------");
+        
+        //Q2
+        for (int i=1; i<=10; i++){
+            System.out.println(i);
+        }
     }
 }
